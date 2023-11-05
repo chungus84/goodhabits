ca8b7d8b93448489676432bb12bae9cb821ed80e
kenichi beveridge <kenbev84@gmail.com>
2023-11-05 19:37:54 +0000

    Added initial setup for frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        environment: 'jsdom',
+        setupFiles: ['./tests/setup.js'],
+        testMatch: ['./tests/**/*.test.jsx'],
+        globals: true,
+        coverage: {
+            provider: 'istanbul',// or 'v8'
+            reporter: ['text', 'json', 'html']
+        },
+
+    }
+
+})
