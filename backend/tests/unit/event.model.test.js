93f4f18544774d239fdb562d0b360602838700da
kenichi beveridge <kenbev84@gmail.com>
2023-11-25 12:38:10 +0000

    Added tests creating valid running and walkign events

--- backend/tests/unit/event.model.test.js
+++ backend/tests/unit/event.model.test.js
@@ -15,4 +15,41 @@ describe('Event Model Tests', () => {
 
         })
     });
+
+    it('should create a new event for Walking', (done) => {
+        const newEvent = new Event({
+            name: "Walking",
+            date: new Date("2023-11-20"),
+            minutes: 52,
+            distance: 2.5
+        });
+
+
+        expect(newEvent).to.have.property('name').to.equal("Walking");
+        expect(newEvent).to.have.property('minutes').to.equal(52);
+        expect(newEvent).to.have.property('distance').to.equal(2.5);
+        expect(newEvent).to.have.property('date').to.be.a('Date');
+        expect(newEvent).to.have.property("_id").to.exist;
+
+        done();
+
+    });
+
+    it('should create a new event for Running', (done) => {
+        const newEvent = new Event({
+            name: "Running",
+            date: new Date("2023-10-13"),
+            minutes: 40,
+            distance: 3
+        });
+
+
+        expect(newEvent).to.have.property('name').to.equal("Running");
+        expect(newEvent).to.have.property('minutes').to.equal(40);
+        expect(newEvent).to.have.property('distance').to.equal(3);
+        expect(newEvent).to.have.property('date').to.be.a('Date');
+        expect(newEvent).to.have.property("_id").to.exist;
+
+        done();
+    });
 })
