f7edc0f9ebf8bb325334c55a6f78feda23dd01e3
kenichi beveridge <kenbev84@gmail.com>
2023-11-26 13:51:52 +0000

    Added addEvent controller tests

--- backend/src/controllers/event.controller.js
+++ backend/src/controllers/event.controller.js
@@ -6,8 +6,17 @@ const eventServices = new EventServices();
 class EventControllers {
     getAllEvents = async (req, res) => {
         try {
-            const events = await HabitServices.getEvents();
+            const events = await eventServices.getEvents();
             res.status(200).json(events);
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    }
+
+    addEvent = async (req, res) => {
+        try {
+            const newEvent = await eventServices.addEvent(req.body)
+            res.status(201).json(newEvent);
         } catch (err) {
             res.status(400).json(err);
         }
--- backend/tests/unit/event.controller.test.js
+++ backend/tests/unit/event.controller.test.js
@@ -9,6 +9,7 @@ chai.use(chaiAsPromised);
 
 import EventServices from "../../src/services/event.service.js";
 import EventControllers from "../../src/controllers/event.controller.js";
+import HabitServices from '../../src/services/habit.service.js';
 
 var sandbox = sinon.createSandbox();
 
@@ -47,9 +48,54 @@ describe('EventController Tests', () => {
             console.log(event);
 
             expect(res.json).to.have.been.calledOnce;
-            // expect(getEventStub).to.have.been.calledOnce;
+            expect(findStub).to.have.been.calledOnce;
         })
 
+        it('should return an error message if promise is rejected', async () => {
+            findStub.restore();
+            getEventStub.restore();
+
+            findStub = sandbox.stub(mongoose.Model, 'find').rejects(sampleEvent);
+
+            const res = {
+                json: sinon.spy(),
+                status: sinon.stub().resolvesThis
+            }
+
+            const response = await eventCont.getAllEvents(null, res);
+            console.log(response);
+            expect(res.json).to.have.been.calledOnce;
+            expect(findStub).to.have.been.calledOnceWith(sampleEvent);
+
+        });
+
+
+
+    })
+
+    describe('addEvent', () => {
+        it('should create a new Event', async () => {
+
+
+            const newEvent = {
+                name: "Walking",
+                date: new Date("2023-11-20"),
+                minutes: 30,
+                distance: 1.5
+            }
+            const res = {
+                json: sinon.spy(),
+                status: sinon.stub().resolvesThis
+            }
+
+            // let addEventStub = sandbox.stub(eventServ, 'addEvent').resolves(newEvent);
+
+            await eventCont.addEvent(newEvent, res);
+
+            expect(res.json).to.have.been.calledOnce;
+            // expect(addEventStub).to.have.been.calledOnce;
+
+        })
     })
 
 
