4497b15ce170c6b86fc9a32f751b7b542eb062f8
kenichi beveridge <kenbev84@gmail.com>
2023-12-01 19:53:11 +0000

    Added tests adding habit to User

--- backend/tests/unit/user.model.test.js
+++ backend/tests/unit/user.model.test.js
@@ -23,12 +23,38 @@ describe('User Model Tests', () => {
 
         done()
 
-    })
+    });
+
+    it('should create another instance of a User', (done) => {
+        const newUser = {
+            firstName: "Joe",
+            lastName: "Bloggs",
+            userName: "BloJoe",
+            email: "joe@test.com",
+            password: "password",
+            repeatedPassword: "password"
+        }
+
+        const user = new User(newUser);
+
+        expect(user).to.have.property("firstName").to.equal(newUser.firstName);
+        expect(user).to.have.property("lastName").to.equal(newUser.lastName);
+        expect(user).to.have.property("userName").to.equal(newUser.userName);
+        expect(user).to.have.property("email").to.equal(newUser.email);
+        expect(user).to.have.property("habits").to.be.a('array').to.be.empty;
+
+        done()
+
+    });
+
+
 
     it('should return errors for each field not filled in', async () => {
         const testUser = new User({})
 
         await testUser.validate().catch(err => {
+            // console.log(err.errors);
+
             expect(err.errors.firstName).to.exist;
             expect(err.errors.lastName).to.exist;
             expect(err.errors.userName).to.exist;
@@ -38,4 +64,60 @@ describe('User Model Tests', () => {
         })
 
     })
+
+    it('should add a new Habit to the habit array', () => {
+        const newUser = {
+            firstName: "Test",
+            lastName: "Testy",
+            userName: "Testy McGee",
+            email: "test@test.com",
+            password: "password",
+            repeatedPassword: "password"
+        }
+
+        const newHabit = {
+            name: "Running",
+            type: "cardio",
+            createdAt: new Date("2022-11-01")
+        }
+
+        const user = new User(newUser);
+
+        user.habits.push(newHabit);
+
+        expect(user.habits[0]).to.have.property('name').to.equal(newHabit.name)
+        expect(user.habits[0]).to.have.property('type').to.equal(newHabit.type)
+        expect(user.habits[0]).to.have.property('events').to.be.empty;
+        expect(user.habits[0]).to.have.property('createdAt').to.be.a('Date')
+
+    })
+
+    it('should produce error messages for malformed habit', async () => {
+        const newUser = {
+            firstName: "Test",
+            lastName: "Testy",
+            userName: "Testy McGee",
+            email: "test@test.com",
+            password: "password",
+            repeatedPassword: "password"
+        }
+
+        const failedHabit = {
+            name: ""
+        }
+
+        const user = new User(newUser);
+
+        user.habits.push(failedHabit)
+
+        await user.validate().catch(err => {
+
+            expect(err.errors['habits.0.type']).to.exist
+            expect(err.errors['habits.0.name']).to.exist
+        })
+
+
+    })
+
+
 })
