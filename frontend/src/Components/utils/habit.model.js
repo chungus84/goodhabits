23633442f3a1f29fbc71210e940cb4f43d77fb17
kenichi beveridge <kenbev84@gmail.com>
2024-01-11 13:24:53 +0000

    Cleaned up HabitModel

--- frontend/src/Components/utils/habit.model.js
+++ frontend/src/Components/utils/habit.model.js
@@ -1,10 +1,10 @@
 export default class HabitModel {
+
     constructor(_id, name, type, createdAt, userId) {
         this._id = _id;
         this.name = name;
         this.type = type;
         this.createdAt = createdAt;
         this.userId = userId
-
     }
 }
