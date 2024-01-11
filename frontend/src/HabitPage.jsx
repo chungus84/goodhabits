5542b4687ad9b6cc4e2ff51cff26cdc78d7f6878
kenichi beveridge <kenbev84@gmail.com>
2024-01-11 13:22:22 +0000

    Cleaned up HabitPage

--- frontend/src/HabitPage.jsx
+++ frontend/src/HabitPage.jsx
@@ -1,25 +1,18 @@
 import PropTypes from 'prop-types';
 
-
 import HabitFeed from './Components/HabitFeed';
 import Modal from './Components/utils/Modal';
 
 const HabitPage = ({ data, submitAction }) => {
 
-    // console.log(data);
-
     const { userHabits, habitCards, error } = data;
-    console.log(userHabits);
-
 
     return (
         <>
             <h2>Habit Page</h2>
             <Modal data={{ userHabits, habitCards, error }} submitAction={submitAction} />
-
             <HabitFeed data={{ userHabits, habitCards, error }} />
         </>
-
     )
 }
 
