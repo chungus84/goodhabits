1d7dd360fb81c80283b112b6111750d2664a575e
kenichi beveridge <kenbev84@gmail.com>
2024-01-11 13:16:50 +0000

    Removed empty spaces from habitAPICalls

--- frontend/asyncFunctions/habitAPICalls.js
+++ frontend/asyncFunctions/habitAPICalls.js
@@ -3,18 +3,12 @@ import axios from 'axios';
 import { authHeader } from './authHeaders.js';
 import { refreshToken } from './refreshToken.js';
 
-
-
 export const getHabitEvents = async userHabitIds => {
-
     try {
-
         const res = await axios.get(`${import.meta.env.VITE_MYDAYSURL}/habits/${userHabitIds.habitId}`, { headers: authHeader() })
         if (Array.isArray(res.data.habits) && res.data.habits.length > 0) return { events: res.data.habits, status: res.status }
         throw new Error("There are no events for this habit, please add one");
-
     } catch (err) {
-
         if (err.response.status === 403) {
             await refreshToken(authHeader());
             const res = await axios.get(`${import.meta.env.VITE_MYDAYSURL}/habits/${userHabitIds.habitId}`, { headers: authHeader() })
@@ -32,14 +26,10 @@ export const getHabitEvents = async userHabitIds => {
 }
 
 export const submitHabit = async habit => {
-
     try {
-
         const res = await axios.post(`${import.meta.env.VITE_MYDAYSURL}/`, habit, { headers: authHeader() })
         return { habit: res.data, status: res.status };
-
     } catch (err) {
-
         if (err.response.status === 403) {
             await refreshToken(authHeader())
             const res = await axios.post(`${import.meta.env.VITE_MYDAYSURL}/`, habit, { headers: authHeader() })
@@ -56,20 +46,15 @@ export const submitHabit = async habit => {
 }
 
 export const submitHabitEvent = async event => {
-
     try {
-
         const res = await axios.post(`${import.meta.env.VITE_MYDAYSURL}/habits/${event.habitId}/add`, event, { headers: authHeader() })
         return { event: res.data, status: res.status };
-
     } catch (err) {
-
         if (err.response.status === 403) {
             await refreshToken(authHeader());
             const res = await axios.post(`${import.meta.env.VITE_MYDAYSURL}/habits/${event.habitId}/add`, event, { headers: authHeader() })
             return { event: res.data, status: res.status };
         }
-
         return {
             status: err.response?.status,
             error: {
@@ -81,18 +66,13 @@ export const submitHabitEvent = async event => {
 }
 
 export const getUser = async (id) => {
-
     try {
-
         const res = await axios.get(`${import.meta.env.VITE_MYDAYSURL}/profile`, { headers: authHeader() })
         return { user: res.data, status: res.status }
-
     } catch (err) {
-
         if (err.response.status === 403) {
             const refreshRes = await refreshToken(authHeader());
             return { user: refreshRes.data, status: refreshRes.status }
-
         }
     }
     return {
@@ -106,12 +86,9 @@ export const getUser = async (id) => {
 
 
 export const addNewUser = async (user) => {
-
     try {
-
         const res = await axios.post(`${import.meta.env.VITE_MYDAYSURL}/profile`, user);
         return { user: res.data, status: res.status }
-
     } catch (err) {
         return {
             status: err.response?.status,
