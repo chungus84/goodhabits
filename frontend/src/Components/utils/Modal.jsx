c0022c5cd4a188c709f1f2d26afcab1a87234f9a
kenichi beveridge <kenbev84@gmail.com>
2024-01-11 13:23:22 +0000

    Cleaned up Modal

--- frontend/src/Components/utils/Modal.jsx
+++ frontend/src/Components/utils/Modal.jsx
@@ -3,27 +3,18 @@ import "./css/Modal.css"
 
 import AddHabit from '../AddHabit'
 
-
 const Modal = ({ data, submitAction }) => {
 
-    // console.log(data);
-    // console.log(submitAction);
-
     const [modal, setModal] = useState(false)
-
     const toggleModal = () => { setModal(!modal) }
-
-
 
     return (
         <>
             <button className={modal ? "btn btn-danger my-3" : "btn btn-primary my-3"} onClick={toggleModal}>{modal ? "Close" : "Add a Habit"}</button>
-
             {modal && (
                 <div className="habit-modal-container">
                     <div className="habit-modal">
                         <div className="habit-modal-header">
-
                         </div>
                         <div className="modal-content">
                             <AddHabit submitAction={submitAction} data={data.userHabits} />
@@ -31,11 +22,7 @@ const Modal = ({ data, submitAction }) => {
                     </div>
                 </div>
             )}
-
-
         </>
-
-
     )
 }
 
