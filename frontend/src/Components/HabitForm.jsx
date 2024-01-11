4905fb756a6e99c737e14758e270bbdddf0c88f2
kenichi beveridge <kenbev84@gmail.com>
2024-01-11 13:27:01 +0000

    Cleaned up HabitForm

--- frontend/src/Components/HabitForm.jsx
+++ frontend/src/Components/HabitForm.jsx
@@ -5,15 +5,11 @@ import './utils/css/HabitForm.css'
 
 const HabitForm = ({ submitAction, data }) => {
 
-    // console.log(data);
-
     const habit = data.habit
 
     const [habitName, setHabitName] = useState("");
     const [habitType, setHabitType] = useState("");
     const [userId, setUserId] = useState("")
-    // const [habitDistance, setHabitDistance] = useState(0);
-    // const [habitCreatedAt, setHabitCreatedAt] = useState("");
 
     const handleSubmit = event => {
         event.preventDefault();
@@ -21,12 +17,7 @@ const HabitForm = ({ submitAction, data }) => {
         setHabitName('');
         setHabitType('');
         setUserId("");
-
-        // setHabitDistance(0);
-        // setHabitCreatedAt("");
     }
-
-    console.log(data);
 
     useEffect(() => {
         setUserId(data.userId)
@@ -35,7 +26,6 @@ const HabitForm = ({ submitAction, data }) => {
     return (
 
         <div className="user-form">
-
             <form aria-label="form" onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="habitName">Habit Name:&nbsp;</label>
@@ -45,15 +35,11 @@ const HabitForm = ({ submitAction, data }) => {
                     <label htmlFor="habitType">Type:&nbsp;</label>
                     <input type="text" name="habitType" id="habitType" placeholder='Type of Habit' data-testid="type" className='form-control' value={habitType} onChange={event => setHabitType(event.target.value)} />
                 </div>
-
                 <div className="form-group">
                     <input type="submit" value="Submit" className={`btn ${!habitName || !habitType ? `btn-danger` : `btn-primary`} my-3`} disabled={!habitName || !habitType} />
                 </div>
-
             </form>
-
         </div>
-
     )
 }
 
