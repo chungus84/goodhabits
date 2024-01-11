540abdc8b79e6d1b975089067cdf5efbdbd38569
kenichi beveridge <kenbev84@gmail.com>
2024-01-11 13:36:33 +0000

    Cleaned up Header

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -4,14 +4,13 @@ import { useNavigate } from 'react-router-dom';
 
 const Header = (data) => {
     const navigate = useNavigate();
-    // console.log(data);
 
     const userName = data.data
+
     return (
         <>
             <h1 className='header-band py-2 px-2' onClick={() => navigate('/')}>goodhabits.</h1>
             <div className="row ">
-
                 <div className='col-9 py-2 my-2 bg-white'></div>
                 <div className="col-3 bg-white my-2">
                     {userName && (
@@ -20,13 +19,9 @@ const Header = (data) => {
                             <span>Hi, {userName}</span>
                             <button className="btn btn-warning mx-2" onClick={data.logout}>Logout</button>
                         </>
-
                     )}
-
                 </div>
-
             </div>
-
         </>
 
     )
