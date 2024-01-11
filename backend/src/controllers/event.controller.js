a9a456c1056c46948cb3e664cd2382816ff54c36
kenichi beveridge <kenbev84@gmail.com>
2024-01-11 13:39:05 +0000

    Cleaned up event.controller

--- backend/src/controllers/event.controller.js
+++ backend/src/controllers/event.controller.js
@@ -1,16 +1,15 @@
 import EventServices from "../services/event.service.js";
-import HabitServices from "../services/habit.service.js";
 
 const eventServices = new EventServices();
 
 class EventControllers {
     getAllEvents = async (req, res) => {
-        // console.log(req.headers.userid);
-        // console.log(req.params);
+
         const userAndHabitId = {
             userId: req.headers.userid,
             habitId: req.params.habitId
         }
+
         try {
             const events = await eventServices.getEvents(userAndHabitId);
             res.status(200).json(events);
