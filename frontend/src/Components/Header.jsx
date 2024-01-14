9775d0d702b6b31d0b4fc54d0047fe892fa9ca6d
kenichi beveridge <kenbev84@gmail.com>
2024-01-14 12:46:37 +0000

    Updated logout button colour

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -9,19 +9,21 @@ const Header = (data) => {
 
     return (
         <>
-            <h1 className='header-band py-2 px-2' onClick={() => navigate('/')}>goodhabits.</h1>
+            <h1 className='header-band py-2 px-2 logo' onClick={() => navigate('/')}>.goodhabits.</h1>
             <div className="row ">
-                <div className='col-9 py-2 my-2 bg-white'></div>
-                <div className="col-3 bg-white my-2">
+                <div className='py-2 my-2 bg-white text-end'>
                     {userName && (
                         <>
                             <img src={userPic} alt="user profile image" className="rounded-circle mx-2" />
                             <span>Hi, {userName}</span>
-                            <button className="btn btn-warning mx-2" onClick={data.logout}>Logout</button>
+                            <button className="btn btn-logout mx-2" onClick={data.logout}>Logout</button>
                         </>
                     )}
+
+
+
                 </div>
-            </div>
+            </div >
         </>
 
     )
